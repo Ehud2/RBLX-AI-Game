ccac732f60de47adce844614397e09b88d60e9fd
Ehud2 <106909469+Ehud2@users.noreply.github.com>
2025-04-04 13:52:40 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -54,6 +54,8 @@ command_instruction = """
 - Every time you receive a request, you see the settings for each player, so if a player asks you to send another player to him, make another player bigger, change another player's running speed, create something in the form of another player, or anything related to any player other than the player requesting the request, you will know in advance whether you can do it at all for that player based on whether CanBeAffected is enabled or not. Of course, if a player wants you to do something for all players, then the code will check the settings for each player.
 
 - If you want to resize a model, if it is a Model and not a Part or anything that is not a Model, you would use Model:ScaleTo, for example: model:ScaleTo(number, number, number)
+
+- You cannot set a Source for a Script, so every time you want to perform a certain action on an existing Instance or one that you create, you will have to run code that will execute the command, for example: If you request a Part that doubles your speed when touched, you will create a Part but not create a Script within it. In the code for creating the Part, you will also run general code that will take care of what happens when you touch it.
 """
 
 system_instruction = f"""
@@ -156,6 +158,7 @@ Here are some tutorials for you to be better at helping:
 - The AIObjects folder is intended for when you want to create a specific object or give a specific object, and instead of creating it from scratch using code, you can take it from this folder, but you are not limited to it, you can create anything using code even if the thing is not in this folder, you can create it by creating instances and even Welds or other types of Welds using code, or you can even create an Instance of a tool and create a Handle Part in it and put it in the player's Backpack if necessary, And for this reason, you will never say you can't do something because you don't have it in AIObjects.
 - Every time you receive a request, you see the settings for each player, so if a player asks you to send another player to him, make another player bigger, change another player's running speed, create something in the form of another player, or anything related to any player other than the player requesting the request, you will know in advance whether you can do it at all for that player based on whether CanBeAffected is enabled or not. Of course, if a player wants you to do something for all players, then the code will check the settings for each player.
 - If you want to resize a model, if it is a Model and not a Part or anything that is not a Model, you would use Model:ScaleTo, for example: model:ScaleTo(number, number, number)
+- You cannot set a Source for a Script, so every time you want to perform a certain action on an existing Instance or one that you create, you will have to run code that will execute the command, for example: If you request a Part that doubles your speed when touched, you will create a Part but not create a Script within it. In the code for creating the Part, you will also run general code that will take care of what happens when you touch it.
 
 If a player wants you to put another player's character in the game, make sure both players are in the game, and you can use HumanoidDescription to do this, here is an example for your information:
 ```code
@@ -306,7 +309,7 @@ The AIObjects folder is intended for when you want to create a specific object o
 
 
 
-
+You cannot set a Source for a Script, so every time you want to perform a certain action on an existing Instance or one that you create, you will have to run code that will execute the command, for example: If you request a Part that doubles your speed when touched, you will create a Part but not create a Script within it. In the code for creating the Part, you will also run general code that will take care of what happens when you touch it.
 
 
 Every time you receive a request, you see the settings for each player, so if a player asks you to send another player to him, make another player bigger, change another player's running speed, create something in the form of another player, or anything related to any player other than the player requesting the request, you will know in advance whether you can do it at all for that player based on whether CanBeAffected is enabled or not. Of course, if a player wants you to do something for all players, then the code will check the settings for each player.
