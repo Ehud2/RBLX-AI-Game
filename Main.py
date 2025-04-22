0d78a31549185cc8bee79395b26dcc930a5bfea1
Ehud2 <106909469+Ehud2@users.noreply.github.com>
2025-04-22 09:02:12 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -510,9 +510,9 @@ def process_queue():
                     last_request_times = [t for t in last_request_times if now - t < 60]
 
                     if len(last_request_times) >= 15:
-                        model_name = "gemini-2.0-flash-lite"
+                        model_name = "gemini-2.5-flash-preview-04-17"
                     else:
-                        model_name = "gemini-2.0-flash"
+                        model_name = "gemini-2.5-flash-preview-04-17"
 
                     last_request_times.append(now)
                     
